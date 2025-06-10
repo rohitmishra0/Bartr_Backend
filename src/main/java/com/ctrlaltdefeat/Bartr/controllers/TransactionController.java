b5693e6ea76f67d79812217276d20518843ba334
= <=>
2025-06-10 10:17:14 +0530

    transaction controller

--- src/main/java/com/ctrlaltdefeat/Bartr/controllers/TransactionController.java
+++ src/main/java/com/ctrlaltdefeat/Bartr/controllers/TransactionController.java
@@ -1,8 +1,44 @@
 package com.ctrlaltdefeat.Bartr.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ctrlaltdefeat.Bartr.Services.TransactionService;
+import com.ctrlaltdefeat.Bartr.models.Transaction;
+
+import io.appwrite.exceptions.AppwriteException;
 
 @RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+    @Autowired
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction){
+        return transactionService.createTransaction(transaction);
+    }
+
+    @GetMapping("/{id}")
+    public Transaction geTransactionById(@PathVariable String id) throws AppwriteException{
+        return transactionService.geTransactionById(id);
+    }
+
+    @GetMapping
+    public List<Transaction> getAllTransactions() throws AppwriteException{
+        return transactionService.getAllTransactions();
+    }
+
+    @DeleteMapping("/id")
+    public void deleteTransaction(@PathVariable String id){
+        transactionService.deleteTransaction(id);
+    }
 
 }
