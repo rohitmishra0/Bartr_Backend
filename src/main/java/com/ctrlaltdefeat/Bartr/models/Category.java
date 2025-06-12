650f9c6ba610dbc1a8fbc1993177a98cb52614d9
= <=>
2025-06-12 11:38:00 +0530

    Changes in id

--- src/main/java/com/ctrlaltdefeat/Bartr/models/Category.java
+++ src/main/java/com/ctrlaltdefeat/Bartr/models/Category.java
@@ -10,10 +10,10 @@ public class Category {
 	private int xp_cost;
 	
 	//Getters and Setters
-	public int getId() {
+	public String getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 	public String getName() {
