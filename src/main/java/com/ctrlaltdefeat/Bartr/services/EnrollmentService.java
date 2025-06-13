e21d617ba8f75398d4c19639c68dc72f17146d90
M <2401627@cognizant.com>
2025-06-13 15:29:05 +0530

    added enroll function

--- src/main/java/com/ctrlaltdefeat/Bartr/services/EnrollmentService.java
+++ src/main/java/com/ctrlaltdefeat/Bartr/services/EnrollmentService.java
@@ -14,6 +14,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+
 @Service
 public class EnrollmentService {
    private final EnrollmentRepository enrollmentRepository;
