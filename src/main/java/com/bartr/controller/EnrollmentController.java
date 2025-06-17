690c20251316120acd11ece2a7dc594adf1f7db8
Anmol <Anmolkr0007@gmail.com>
2025-06-17 23:16:34 +0530

    Controller class updated

--- src/main/java/com/bartr/controller/EnrollmentController.java
+++ src/main/java/com/bartr/controller/EnrollmentController.java
@@ -1,4 +1,40 @@
 package com.bartr.controller;
 
+import com.bartr.model.Course;
+import com.bartr.model.Enrollment;
+import com.bartr.model.User;
+import com.bartr.service.EnrollmentService;
+import com.bartr.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class EnrollmentController {
+    @Autowired
+    private EnrollmentService enrollmentService;
+
+    @PostMapping("insert")
+    public ResponseEntity<User> createEnrollment(@RequestBody Enrollment enrollment) {
+        return ResponseEntity.status(201).body(enrollmentService.createEnrollment(enrollment));
+    }
+
+    @PutMapping("updateById/{id}")
+    public ResponseEntity<Course> updateEnrollment(@PathVariable int id, @RequestBody Enrollment enrollment) {
+        return ResponseEntity.ok(userService.updateEnrollment(id, enrollment));
+    }
+
+    @DeleteMapping("deleteById/{id}")
+    public ResponseEntity<Void> deleteEnrollment(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("getById/{id}")
+    public ResponseEntity<Course> getEnrollmentById(@PathVariable int id) {
+        return ResponseEntity.ok(enrollmentService.getEnrollmentById(id));
+    }
+
+
 }
--- src/main/java/com/bartr/controller/UserController.java
+++ src/main/java/com/bartr/controller/UserController.java
@@ -1,5 +1,45 @@
 package com.bartr.controller;
 
+import com.bartr.model.Course;
+import com.bartr.model.User;
+import com.bartr.service.CourseService;
+import com.bartr.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class UserController {
-    
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("insert")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.status(201).body(userService.createUser(user));
+    }
+
+    @PutMapping("updateById/{id}")
+    public ResponseEntity<Course> updateCourse(@PathVariable int id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.updateCourse(id, user));
+    }
+
+    @DeleteMapping("deleteById/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("getById/{id}")
+    public ResponseEntity<Course> getUserById(@PathVariable int id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Course>> getAllCourses() {
+        return ResponseEntity.ok(courseService.getAllCourses());
+    }
+
+
 }
