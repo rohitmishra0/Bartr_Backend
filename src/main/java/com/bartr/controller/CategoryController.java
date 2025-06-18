1ac9af811ad0590924b9616a2f53f139c03ec387
= <=>
2025-06-18 16:16:56 +0530

    Changed package name from batr to bartr

--- src/main/java/com/bartr/controller/CategoryController.java
+++ src/main/java/com/bartr/controller/CategoryController.java
@@ -4,22 +4,14 @@ package com.bartr.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.bartr.model.Category;
 import com.bartr.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
+@CrossOrigin("http://localhost:4200/")
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
