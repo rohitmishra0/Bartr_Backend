581e1d7bad4cb008d9d833cc07bc5bf22129412f
M <2401627@cognizant.com>
2025-06-30 12:41:59 +0530

    exception handling

--- src/main/java/com/bartr/service/impl/SearchServiceImpl.java
+++ src/main/java/com/bartr/service/impl/SearchServiceImpl.java
@@ -1,7 +1,10 @@
 package com.bartr.service.impl;
 
+import com.bartr.exception.UserNameNotFoundException;
 import com.bartr.model.Course;
+import com.bartr.model.User;
 import com.bartr.repository.CourseRepository;
+import com.bartr.repository.UserRepository;
 import com.bartr.service.SearchService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +16,12 @@ import java.util.List;
 public class SearchServiceImpl implements SearchService {
 
     private final CourseRepository courseRepository;
+    private final UserRepository userRepository;
 
     @Override
     public List<Course> search(String keyword, int userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNameNotFoundException("User not found"));
         return courseRepository.searchRelevantCourses(keyword,userId);
     }
 }
